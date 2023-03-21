fd8f233e469fd7ebe9ea40ca1bf4a44acf6cfc0d
Administrator <Administrator@127.0.0.1>
2023-03-21 22:18:45 +0900

    added Menumenager.java

---
+++ src/file/Menumenager.java
@@ -0,0 +1,13 @@
+package file;
+
+public class Menumenager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Books");
+		System.out.println("2. Delete Books");
+		System.out.println("3. Edit Books");
+		System.out.println("4. View Books");
+		System.out.println("5. Exit");
+	}
+
+}
